5f1b876e35e5e3055c323714509dc3bbf3f9267d
ashesh gupta <guptaashesh180@gmail.com>
2025-11-17 22:13:46 +0530

    python day1 done

---
+++ python fundememtal part1/assignment.py
@@ -0,0 +1,98 @@
+# WAP to ask user age and name and print in string
+
+# name  = input("enter name ")
+# age = input("enter age ")
+
+# print("hello ",name,", you are ",age,"years old!")
+
+
+# take two number as input form the user and print their sum, differencem product and quotient. 
+
+# num1 = int(input("enter num1 "))
+# num2 = int(input("enter num2 "))
+
+# print("sum is = ", num1+num2)
+# print("diff is = ", num1-num2)
+# print("mul is = ", num1*num2)
+# print("quotient is = ", num1/num2)
+
+# Ask the user to enter two integers and one float. Convert them all to floats and print their average.
+
+# num1 = input("enter integrs = ")
+# num2 = input("enter integrs = ")
+# num3 = input("enter float = ")
+
+# num1 =  float(num1)
+# num2 =  float(num2)
+# num3 =  float(num3)
+
+# avg = (num1 + num2 + num3)/3
+# print("average is = ", avg)
+
+
+# the use enters strng containing a nuber (e.g "45") and convert them into integer, float and back to string. 
+
+# num  = input("enter the number in string = ")
+
+# print(type(int(num)), num)
+# print(type(float(num)), num)
+# print(type(str(num)), num)
+
+
+# evaluate and print the result of the following exression:   x = 10 + 3 * 2 ** 2
+
+# answer = 22
+
+# write a program to swap 2 numbers entered by user 
+
+# num1 = input("enter the num1 ")
+# num2 = input("enter the num2 ")
+
+# #swap loging
+# temp = num1 
+# num1 = num2
+# num2 = temp
+
+# print("num1 = ",  num1,  "num2 = ", num2)
+
+# ask temp in celsius (string input). convert it to float and then calculate and print temp in fahrenheight. 
+
+# formula for conversion - farenhiteTemp =  (celsiusTemp * (9/5)) +  32
+
+# CelsiusTemp   = input("enter temp in celsius = ")
+# CelsiusTemp = float(CelsiusTemp)
+
+# fahrenheitTemp =  (CelsiusTemp * (9/5)) + 32
+
+# print(fahrenheitTemp)
+
+# take radius r and print area of circle
+
+# radius = input("enter the radius = ")
+# radius =  int(radius)
+# area = 3.14 * radius * radius
+# print(area)
+
+
+# ask for principle rate and time and convert them into float and print simple intrest of them 
+
+# principle = float(input("enter the princple = ") )
+# rate = float(input("enter the rate = "))
+# time = float(input("enter the time = "))
+
+# SI = (principle * rate * time)/100
+# print(SI)
+
+# take a decimal number as input (like 45.78) and outpit is :
+# integer part = 45
+# decimal part = .78
+
+# number = float(input("Enter a decimal number: "))
+# integer_part = int(number)
+
+# # This trick: multiply by 100, subtract integer*100, divide by 100
+# fractional_part = (number * 100 - integer_part * 100) / 100
+
+# print("Integer part:", integer_part)
+# print("Fractional part:", fractional_part)
+
---
+++ python fundememtal part1/code.py
@@ -0,0 +1,115 @@
+# here this print is function and "hello world" is a string
+# print("hello world")
+# for next line we have \n
+# print("hello \nworld")
+
+# VARIABLE
+# name of our memory address where our data is stored 
+# eg. age = 12 where age is variable name and it is also called identifiers.
+
+# name = "ashes"
+# age = 23
+# pi = 3.14
+
+# print(name, age, pi)
+
+#-------------------------------------------------------------------------
+
+# DATA TYPES in python 
+
+# integer --> =ve, -ve, 0
+# Strning --> ''. " ", "'  '"
+# Float --> decimal
+# Boolean --> True and False
+# None  --> if we dont want to assign value to variable so we give none
+
+#-------------------------------------------------------------------------
+
+
+# Keyword --> these are reserved words means they have fixed meaning in python
+# some example of Keyowrds are -- > Fasle None True if  else etc..
+
+#-------------------------------------------------------------------------
+
+# comments   denoted with # --> only ment for human.
+
+'''this is a multime line comment'''
+#-------------------------------------------------------------------------
+
+# style guide 
+
+    # snake case  --> total_price
+    # camel case  --> totalPrice
+    # pascle case  --> TotalPrice
+
+#-------------------------------------------------------------------------
+
+# Operators
+    # Arthematic   --> +, -, *, /, %, **
+    # Relatopnal / Comprasion  --> ==, <, <=, >, >=, !=  
+    # Assignment --> = . +=. -=
+    # Logical --> not,  and, or
+    # bitwise 
+    # membership
+
+
+#-------------------------------------------------------------------------
+
+# Operator precedence 
+
+# priority of operators 
+
+    # () >> **  >>  *,/,%  >>  +,-  >>  ==, !=, >, >= ,<, <=   >>    not >> and >> or
+
+# note if some operators have same presidence then we solve form left ot right. 
+
+#-------------------------------------------------------------------------
+
+# Type Conversion
+
+# It means to convert one datatype into another datatype
+
+# It has 2 type 
+    # type conversion ( implecit means convert by python )
+    # type casting ( explecit means convert by developer )
+
+# ans1 = int(5 + 10.0) #casting
+# ans2 = 5 + 10.0  #conversion
+
+# print(ans1, type(ans1))
+# print(ans2, type(ans2))
+
+
+#-------------------------------------------------------------------------
+
+# User Input
+
+# input()
+
+# NOTE whatever value we take it is string value.
+# a = input("enter value of a = ")
+# print(a)
+
+
+#-------------------------------------------------------------------------
+
+# sum of 2 number 
+
+# a = int(input("enter value of a "))
+# b = int(input("enter value of b "))
+
+# sum = a+b
+
+# print(sum)
+
+#-------------------------------------------------------------------------
+
+# Avg or 2 number 
+
+# a = int(input("enter value of a "))
+# b = int(input("enter value of b "))
+
+# avg = (a+b)/2
+
+# print("avg of 2 number is = ",avg)
+
